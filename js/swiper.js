0696e4dbbc32e37a516f22d3589432296c726288
Danila Kanapatski <danila_konopatskiy@mail.ru>
2023-10-11 00:38:06 +0300

    fixed mistakes

---
+++ js/swiper.js
@@ -0,0 +1,14 @@
+new Swiper('.swiper', {
+    speed: 700,
+    // spaceBetween: 100,
+    slideToClickedSlide: true,
+    keyboard: {
+        enabled: true
+    }, 
+    autoHeight: true,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    }
+});

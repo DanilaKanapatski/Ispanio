5a5bd6f9035c65c56a1602b8d356e61447661a16
Danila Kanapatski <danila_konopatskiy@mail.ru>
2023-10-12 00:34:44 +0300

    added 2 blocks

--- js/swiper.js
+++ js/swiper.js
@@ -1,14 +1,46 @@
-new Swiper('.swiper', {
+new Swiper('.swiper-1', {
     speed: 700,
     // spaceBetween: 100,
     slideToClickedSlide: true,
     keyboard: {
         enabled: true
-    }, 
+    },
     autoHeight: true,
     loop: true,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    }
+    // autoplay: {
+    //     delay: 3000,
+    //     disableOnInteraction: false,
+    // },
+    // slidesPerView: 'auto',
+    // slidesPerGroup: 1,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+    },
+});
+
+new Swiper('.swiper-2', {
+    speed: 700,
+    spaceBetween: 40,
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+    slideToClickedSlide: true,
+    keyboard: {
+        enabled: true
+    },
+    // autoHeight: true,
+    // loop: true,
+    // autoplay: {
+    //     delay: 3000,
+    //     disableOnInteraction: false,
+    // },
+    slidesPerView: '1.4',
+    // slidesPerGroup: 1.5,
+
 });
